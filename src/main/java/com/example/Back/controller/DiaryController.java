8e7427fdc2b6ee44a22bd9d3b019b749e0806654
조준희 <junh9541@gmail.com>
2024-10-15 17:47:53 +0900

    feat: swagger-ui 설정

--- src/main/java/com/example/Back/controller/DiaryController.java
+++ src/main/java/com/example/Back/controller/DiaryController.java
@@ -7,6 +7,8 @@ import com.example.Back.entity.Member;
 import com.example.Back.service.DiaryService;
 import com.example.Back.service.MemberService;
 import com.example.Back.util.ResponseUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -25,15 +27,17 @@ public class DiaryController {
         this.diaryService = diaryService;
         this.memberService = memberService;
     }
-    //    일기 조회
-    //    유저 이메일 + 날짜
+
+    @Operation(summary = "일기 조회 API")
     @GetMapping("/{date}/{email}")
-    public ResponseDto.Response<DiaryDto.Response> getDiary(@PathVariable("date") String date, @PathVariable("email") String email) throws ParseException {
+    public ResponseDto.Response<DiaryDto.Response> getDiary(
+            @Parameter(description = "작성 날짜", example = "2024-10-15") @PathVariable("date") String date,
+            @Parameter(description = "사용자 이메일", example = "email1@mail.com") @PathVariable("email") String email) throws ParseException {
         Diary diary= diaryService.get(date, email).orElseThrow();
         return ResponseUtils.ok(new DiaryDto.Response(diary));
     }
 
-//    일기 등록, 수정
+    @Operation(summary = "일기 작성 API")
     @PostMapping("/post")
     public ResponseDto.Response<ResponseDto.Success> postDiary(@RequestBody DiaryDto.Request diaryReqDto) throws ParseException {
         Optional<Diary> diary= diaryService.get(diaryReqDto.getDate(), diaryReqDto.getEmail());
@@ -57,8 +61,11 @@ public class DiaryController {
         return ResponseUtils.ok(new ResponseDto.Success("일기 작성 완료"));
     }
 
+    @Operation(summary = "일기 삭제 API")
     @PostMapping("/delete")
-    public ResponseDto.Response<ResponseDto.Success> deleteDiary(@RequestParam("date") String date, @RequestParam("email") String email) throws ParseException {
+    public ResponseDto.Response<ResponseDto.Success> deleteDiary(
+            @Parameter(description = "작성 날짜", example = "2024-10-15") @RequestParam("date") String date,
+            @Parameter(description = "사용자 이메일", example = "email2@mail.com") @RequestParam("email") String email) throws ParseException {
         diaryService.delete(date, email);
         return ResponseUtils.ok(new ResponseDto.Success("삭제 완료"));
     }
