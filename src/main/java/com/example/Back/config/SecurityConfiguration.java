94021994c2e8c208dba44ea524d671366f312996
조준희 <junh9541@gmail.com>
2024-10-16 14:29:23 +0900

    SecurityConfig 기본 설정

---
+++ src/main/java/com/example/Back/config/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.example.Back.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration{
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .oauth2Login(Customizer.withDefaults())
+//                .authorizeHttpRequests((auth)->{
+//                    auth.anyRequest().authenticated();
+//                })
+                .sessionManagement((session)->{
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
+                .build();
+
+    }
+}
