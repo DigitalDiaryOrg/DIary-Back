d661e8bdf549727cf046627681dbbc93d54aac73
안정길 <jeonggilahn@gmail.com>
2024-10-30 16:53:13 +0900

    feat: 로그인 성공시 jwt 토큰 발급, 쿠키 전달

--- src/main/java/com/example/Back/config/SecurityConfiguration.java
+++ src/main/java/com/example/Back/config/SecurityConfiguration.java
@@ -1,6 +1,9 @@
 package com.example.Back.config;
 
+import com.example.Back.jwt.JWTUtil;
+import com.example.Back.oauth2.CustomSuccessHandler;
 import com.example.Back.service.CustomOAuth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -15,10 +18,19 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfiguration{
 
+    @Autowired
     private final CustomOAuth2UserService customOAuth2UserService;
+    @Autowired
+    private final CustomSuccessHandler customSuccessHandler;
+    @Autowired
+    private final JWTUtil jwtUtil;
 
-    public SecurityConfiguration(CustomOAuth2UserService customOAuth2UserService){
+    public SecurityConfiguration(CustomOAuth2UserService customOAuth2UserService,
+                                 CustomSuccessHandler customSuccessHandler,
+                                 JWTUtil jwtUtil){
         this.customOAuth2UserService = customOAuth2UserService;
+        this.customSuccessHandler = customSuccessHandler;
+        this.jwtUtil = jwtUtil;
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -32,16 +44,17 @@ public class SecurityConfiguration{
                 .headers((headerConfig)->{
                     headerConfig.frameOptions((HeadersConfigurer.FrameOptionsConfig::sameOrigin));
                 })
-
                 .oauth2Login(Customizer.withDefaults())
 
                 .authorizeHttpRequests((auth)->{auth
                         .requestMatchers("/auth/test/**").authenticated()
                         .anyRequest().permitAll();
                 })
+//                로그인
                 .oauth2Login((oauth2)->oauth2
                         .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
                                 .userService(customOAuth2UserService))
+                                .successHandler(customSuccessHandler)
                         )
                 .sessionManagement((session)->{
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
